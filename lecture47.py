dcf67771017ac275862321dcae3363657002fb74
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2022-11-02 14:02:22 +0530

    Add files via upload

---
+++ lecture47.py
@@ -0,0 +1,8 @@
+'''
+Join function - To join the elements of the list together with some common string in between we use join function
+'''
+
+lis = ['John Cena', 'Randy Orton', 'Shemus', 'Khali', 'Jinder Mahal']
+
+a = ', '.join(lis) # to put comma in between the elements of the list we can actually use the join command
+print(f'{a} and other wwe stars')
