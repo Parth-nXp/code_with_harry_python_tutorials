176b75401be2ff2e33bd4d6559c852caa586797a
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2022-11-02 13:48:16 +0530

    Add files via upload

---
+++ lecture46_version2.py
@@ -0,0 +1,7 @@
+import lecture46_version1
+
+
+# if __name__ == '__main__' is not used in lecture46_version1 then the complete program will be executed while importing.
+# so we can actually see some content from the lecture46_version1 which we dont want to display
+print(lecture46_version1.printhar('rajjo'))
+print(lecture46_version1.addnum(1,5))
