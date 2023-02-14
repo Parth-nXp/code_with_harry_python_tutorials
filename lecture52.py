1278631db3e513cc3dd5b91c6d25c4ef3bb4fe23
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2023-02-14 11:51:54 +0530

    Add files via upload

---
+++ lecture52.py
@@ -0,0 +1,14 @@
+'''
+Classes & Objects (OOPS): Object oriented programming is a way of programming by creating objects.  
+Object is something which is derived from the class template.
+
+class is like a template.
+object is something which is created using this template of class. Objhect is sometime also regraded as instance of class.
+
+Why we create a class?
+Because the OOPS followss the DRY concept which is do not repeat yourself. OOPS helps in saving the time and efforts.
+
+difference between the function and the class is that class restricts the access of the function.
+
+'''
+
